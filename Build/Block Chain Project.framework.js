b30503ae9c0dc6191aea1da401312275cc521fa3
androsanz <androsanz19@gmail.com>
2024-09-18 12:53:20 -0400

    Auth

--- Build/Block Chain Project.framework.js
+++ Build/Block Chain Project.framework.js
@@ -1993,13 +1993,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4637296: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4637357: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4637421: function() {return Module.webglContextAttributes.powerPreference;},  
- 4637479: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4637534: function($0) {performance.now = function() { return $0; };},  
- 4637582: function($0) {performance.now = function() { return $0; };},  
- 4637630: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4637360: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4637421: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4637485: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4637543: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4637598: function($0) {performance.now = function() { return $0; };},  
+ 4637646: function($0) {performance.now = function() { return $0; };},  
+ 4637694: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
